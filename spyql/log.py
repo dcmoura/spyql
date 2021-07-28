b0eb1442cf38c366a9f8850b602980d0ea7dc1f6
dcmoura <dmoura@veniam.com>
2021-07-28 13:36:04 +0100

    cleanup of logging code

--- spyql/log.py
+++ spyql/log.py
@@ -3,7 +3,22 @@ import asciichartpy as colors
 import sys
 import json
 
-error_on_warning = False
+error_on_warning = False  # TODO command line arg
+
+
+def colored(val, color):
+    """
+    Creates string representation of `val` for color printing
+    """
+    return color + str(val) + colors.reset
+
+
+def mk_user_msg(level, level_color, message, code=None, code_color=colors.lightyellow):
+    return (
+        colored(level, level_color)
+        + f"\t{message}"
+        + f"{': ' + colored(code, code_color) if code else ''}"
+    )
 
 
 def user_error(message, exception, code=None, vars=None):
@@ -13,13 +28,11 @@ def user_error(message, exception, code=None, vars=None):
     Prints the data that originated the exception (if available).
     """
     sys.tracebacklimit = 0
-    logging.error(
-        f"{colors.lightred}ERROR{colors.reset}\t{message}{': ' + colors.lightyellow + str(code) + colors.reset if code else ''}"
-    )
+    logging.error(mk_user_msg("ERROR", colors.lightred, message, code))
     if vars and vars.get("input_row_number") and vars.get("_values"):
         logging.error(
-            f"\tat data row #{vars['input_row_number']}:"
-            f" {colors.darkgray}{vars['_values']}{colors.reset}"
+            f"\tat data row #{vars['input_row_number']}: "
+            + colored(vars["_values"], colors.darkgray)
         )
     raise exception from None
 
@@ -34,18 +47,14 @@ def user_warning(message, exception=None, code=None):
         if exception:
             raise exception
         raise Exception(message)
-    logging.warning(
-        f"{colors.lightyellow}WARNING{colors.reset}\t{message}{': ' + colors.lightyellow + str(code) + colors.reset if code else ''}"
-    )
+    logging.warning(mk_user_msg("WARNING", colors.lightyellow, message, code))
 
 
 def user_info(message, code=None):
     """
     Reports (verbose) information.
     """
-    logging.info(
-        f"{colors.green}INFO{colors.reset}\t{message}{': ' + colors.yellow + str(code) + colors.reset if code else ''}"
-    )
+    logging.info(mk_user_msg("INFO", colors.green, message, code, colors.yellow))
 
 
 def user_debug(message, code=None):
@@ -53,7 +62,7 @@ def user_debug(message, code=None):
     Reports (verbose) information.
     """
     logging.debug(
-        f"{colors.lightcyan}DEBUG{colors.reset}\t{message}{': ' + colors.lightgray + str(code) + colors.reset if code else ''}"
+        mk_user_msg("DEBUG", colors.lightcyan, message, code, colors.lightgray)
     )
 
 
