6142c8d72a02855aca6e026bbb9ebf59ae32cbd5
dcmoura <dmoura@veniam.com>
2021-10-23 22:59:41 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
         "Documentation": "https://spyql.readthedocs.io",
         "Source": "https://github.com/dcmoura/spyql",
     },
-    version="0.1.0",
+    version="0.2.0",
     zip_safe=False,
 )
